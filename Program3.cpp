37fb9b9aeab7bf3c9b47c0d3e7f8d21bcb674c37
Shubham997799 <136079809+Shubham997799@users.noreply.github.com>
2024-02-15 23:33:53 +0530

    Create Program3.cpp

---
+++ Program3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout<<"Enter the 1st no : \n";
+    cin>>a;
+
+    cout<<"Enter the 2nd no : \n";
+    cin>>b;
+
+    c=a+b;
+
+    cout<<"Addition :"<<c<<"\n";
+
+
+    return 0;
+}
